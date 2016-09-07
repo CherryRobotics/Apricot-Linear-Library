a91cd6e4495f206ec79d670e7d715d775cf8b9a2
Alex Gaesser <agaesser@gmail.com>
2016-09-07 16:41:06 -0700

    overloaded ==

--- apricotVector.h
+++ apricotVector.h
@@ -11,17 +11,22 @@ class ApricotVector {
     ApricotVector(int n, ...);
     ApricotVector(std::string s);
     ~ApricotVector();
+
     void add(std::vector<double> v);
     void add(ApricotVector av);
     void sub(std::vector<double> v);
     void sub(ApricotVector av);
     void normalize();
     void scale(double scalar);
+
     bool ParallelTo(ApricotVector av);
     bool OrthoganalTo(ApricotVector av);
     ApricotVector ProjectedOn(ApricotVector av);
+
     std::vector<double> GetVector() const;
     friend std::ostream &operator<<(std::ostream &, const ApricotVector &);
+    friend bool operator==(const ApricotVector&, const ApricotVector&);
+
     double GetMagnitude();
     double DotProduct(ApricotVector av);
     double getAngleBetween(ApricotVector av, bool inDegrees);
--- main.cpp
+++ main.cpp
@@ -52,7 +52,25 @@ int main(int argc, char* argv[]) {
   ApricotMatrix am = ApricotMatrix("[[1, 0, -2], [-1, -4, 5], [1, 1, 1]]");
   cout << am << "\n\n\n";
 
+  ApricotVector v0 = ApricotVector("[1,2,3]");
+  ApricotVector v1 = ApricotVector("[1,2,3]");
+  ApricotVector v2 = ApricotVector("[9,8,2]");
+  ApricotVector v3 = ApricotVector("[1,2,3,7]");
 
+  cout << "Comparing equality, should return TRUE" << endl;
+  if (v0 == v1) {
+    cout << "TRUE" << endl;
+  }
+
+  cout << "Comparing equality, no return" << endl;
+  if (v0 == v2) {
+    cout << "FALSE" << endl;
+  }
+
+  cout << "Comparing equality with vectors of different sizes, should return size mismatch message" << endl;
+  if (v0 == v3) {
+    cout << "FALSE" << endl;
+  }
 
   return 0;
 }
