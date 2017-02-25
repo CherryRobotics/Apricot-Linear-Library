5a92ae12f114380983aff561c7cc7d6d7d92b53b
KadynCBR <kadyn_martinez@hotmail.com>
2017-02-24 23:15:22 -0800

    we don't need colors.

--- src/main.cpp
+++ src/main.cpp
@@ -12,43 +12,6 @@
 #include <sstream>
 
 using namespace std;
-enum Color {
-  NONE = 0,
-  BLACK, RED, GREEN,
-  YELLOW, BLUE, MAGENTA,
-  CYAN, WHITE
-};
-
-std::string set_color(Color foreground = NONE, Color background = NONE) {
-  std::stringstream s;
-  s << "\033[";
-  if (!foreground && !background) {
-    s << "0";
-  }
-  if (foreground) {
-    s << 29 + foreground;
-    if(background) s <<";";
-  }
-  if (background) {
-    s<<39 + background;
-  }
-  s << "m";
-  return s.str();
-}
-void passed(std::string testname, bool passed) {
-  if (passed) {
-    cout << set_color(GREEN) <<
-    "------------------------------------" << endl<<
-    testname << "         OK!" << endl<<
-    "------------------------------------" << set_color() << endl;
-  } else {
-    cout << set_color(RED) <<
-    "------------------------------------" << endl <<
-    testname << "         FAILED!" << endl<<
-    "------------------------------------" << set_color() << endl;
-  }
-}
-
 
 int main(int argc, char* argv[]) {
   ApricotLine al = ApricotLine(ApricotVector("[2, 3]"), 6);
